d48e1ebfd5d3a10023986c305bb60dee821587af
Kehar Kumar Ojha <keharojha56@gmail.com>
2025-01-22 11:50:48 +0545

    changes added

--- src/protected_routes/UserRoutes.jsx
+++ src/protected_routes/UserRoutes.jsx
@@ -1,18 +1,12 @@
-import React from 'react'
-import { Outlet, Navigate } from 'react-router-dom'
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const UserRoutes = () => {
+  // Get user data from local storage
+  const user = JSON.parse(localStorage.getItem("user"));
 
-      // 1. Get user data from local storage
-      const user = JSON.parse(localStorage.getItem('user'))
+  // Check user and ensure they are not an admin
+  return user && !user.isAdmin ? <Outlet /> : <Navigate to={"/login"} />;
+};
 
-      // check user
-      // Check isAdmin= true
-      // if true : Access all the routes of Admin (Outlet)
-      // if false : Navigate to login page
-  
-      return user != null  ? <Outlet />
-          : <Navigate to={'/login'} />
-}
-
-export default UserRoutes
+export default UserRoutes;
